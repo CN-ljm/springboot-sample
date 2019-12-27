66acbefcd3c81b4d3ede070ff55807faf5a3e892
CN-ljm <759756407@qq.com>
2019-12-27 20:54:20 +0800

    调整了依赖---

--- src/main/java/com/ljm/Application.java
+++ src/main/java/com/ljm/Application.java
@@ -2,6 +2,7 @@ package com.ljm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 应用启动类
